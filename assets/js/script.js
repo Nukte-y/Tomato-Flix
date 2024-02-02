42903e6cd8457b92eee5eaabd823f0243af29a26
nukte-y <nukteu@gmail.com>
2024-02-02 18:09:09 +0000

    added event and function to display article titles

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
-$("#search-btn").on("click", function (event) {
+var title;
+$("#search-btn").on("click", function searchMovie(event) {
   event.preventDefault(); 
-  $("#search-btn").empty();
+  $("#search-btn").empty();       //cleaning sections
   $("#film-Info").empty();
-  var title = $(".form-control").val();
+  $("#article-Display").empty();
+  title = $(".form-control").val();
 
   fetch("https://www.omdbapi.com/?t=" + title + "&plot=full&apikey=trilogy")
     .then(function(response) {
@@ -16,27 +18,36 @@ $("#search-btn").on("click", function (event) {
       // Call the function with the genre
       playSound(genre);
     });
+  return title;
+});
 
-    var apiURL = "https://gnews.io/api/v4/search?";
-    var key = "&country=us&max=10&token=d06b56befd778f95afde57c26ebc9890";
-    searchString = title +" movie";
-    articleURL = apiURL + "q=" + searchString + key;
-    fetch(articleURL)
-      .then(function (response) {
-      return response.json();
-      }).then(function (data) {
-        console.log(data);
-      var article=$("<div>").text(data.articles[0].content);
-      console.log(data.articles[0]);  
-      $("#article-Display").prepend(article);
-    })
+function playSound(theme) {               //plays music that matches the genre
+  var movieSound = $("<audio>").appendTo("#search");
+  movieSound.attr("src", `./assets/musics/${theme}.mp3`);
+  movieSound.get(0).play();
+  $("#pause-button").on("click", function() {
+    movieSound.get(0).pause();
   });
-  function playSound(theme) {
-    var movieSound = $("<audio>").appendTo("#search");
-    movieSound.attr("src", `./assets/musics/${theme}.mp3`);
-    movieSound.get(0).play();
-    $("#pause-button").on("click", function() {
-      movieSound.get(0).pause();
-    });
-  };
-    
+};
+  
+$("#news-btn").on("click",displayArticle)
+
+var apiURL = "https://gnews.io/api/v4/search?";
+var key = "&country=us&max=10&token=d06b56befd778f95afde57c26ebc9890";
+searchString = title +" movie";
+articleURL = apiURL + "q=" + searchString + key;
+
+function displayArticle(event){       //displays article titles
+  event.preventDefault();
+  $("#article-Display").empty();
+  fetch(articleURL)
+    .then(function (response) {
+    return response.json();
+    }).then(function (data) {
+      for(var i=0;i<5;i++){
+        var articleUrl=data.articles[i].url;
+        var articleTitle=$("<div>").append($("<a>").text(data.articles[i].title).attr({"href":articleUrl,"target":"_blank"})); 
+        $("#article-Display").append(articleTitle);
+      }
+    }
+)};
